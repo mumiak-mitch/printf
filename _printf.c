3d04a5011100cc72fe1bd675450e90ecdc4e85b1
mumiak-mitch <akoth0mitch@gmail.com>
2024-01-30 11:36:13 -0800

    you have to earn it in order to own it

--- _printf.c
+++ _printf.c
@@ -1,6 +1,5 @@
 #include <stdarg.h>
 #include <unistd.h>
-#include "main.h"
 
 /**
  * _printf - Custom printf function
@@ -8,19 +7,10 @@
  *
  * Return: Number of characters printed (excluding null byte)
  */
- 
-void print_hexadecimal(unsigned long num, int base);
-int hex_length(unsigned long num);
-void print_special_string(char *str);
-int special_string_length(char *str);
-void print_rot13(char *str);
-int string_length(char *str);
-
 int _printf(const char *format, ...)
 {
     int specific_characters = 0;
     va_list my_list;
-    int num;
 
     if (format == NULL)
         return (-1);
@@ -57,11 +47,23 @@ int _printf(const char *format, ...)
                 write(1, str, strlen);
                 specific_characters += strlen;
             }
-            else if (*format == 'u')
+            else if (*format == '%')
             {
-                unsigned int num = va_arg(my_list, unsigned int);
+                write(1, "%", 1);
+                specific_characters++;
+            }
+            else if (*format == 'd' || *format == 'i')
+            {
+                int num = va_arg(my_list, int);
                 char buffer[12];
                 int length = 0;
+
+                if (num < 0)
+                {
+                    write(1, "-", 1);
+                    specific_characters++;
+                    num = -num;
+                }
 
                 if (num == 0)
                 {
@@ -83,101 +85,6 @@ int _printf(const char *format, ...)
                     }
                 }
             }
-            else if (*format == 'd' || *format == 'i')
-            {
-                if (num < 0)
-                {
-                    write(1, "-", 1);
-                    specific_characters++;
-                    num = -num;
-                }
-            }
-            else if (*format == 'x' || *format == 'X')
-            {
-               
-                unsigned int num = va_arg(my_list, unsigned int);
-                char buffer[12]; 
-                int length = 0;
-                char hex_chars[] = "0123456789abcdef";
-
-                if (num == 0)
-                {
-                    write(1, "0", 1);
-                    specific_characters++;
-                }
-                else
-                {
-                    while (num != 0)
-                    {
-                        buffer[length++] = hex_chars[num % 16];
-                        num /= 16;
-                    }
-
-                    while (length > 0)
-                    {
-                        write(1, &buffer[--length], 1);
-                        specific_characters++;
-                    }
-                }
-            }
-            else if (*format == 'b')
-            {
-                
-                unsigned int num = va_arg(my_list, unsigned int);
-                print_binary(num);
-                specific_characters += binary_length(num);
-            }
-            else if (*format == 'o')
-            {
-                
-                unsigned int num = va_arg(my_list, unsigned int);
-                char buffer[12]; 
-                int length = 0;
-
-                if (num == 0)
-                {
-                    write(1, "0", 1);
-                    specific_characters++;
-                }
-                else
-                {
-                    while (num != 0)
-                    {
-                        buffer[length++] = num % 8 + '0';
-                        num /= 8;
-                    }
-
-                    while (length > 0)
-                    {
-                        write(1, &buffer[--length], 1);
-                        specific_characters++;
-                    }
-                }
-            }
-            else if (*format == 'p')
-            {
-                
-                void *ptr = va_arg(my_list, void *);
-                write(1, "0x", 2);
-                specific_characters += 2;
-                print_hexadecimal((unsigned long)ptr, 16);
-                specific_characters += hex_length((unsigned long)ptr);
-            }
-            else if (*format == 'S')
-            {
-                
-                char *str = va_arg(my_list, char*);
-                print_special_string(str);
-                specific_characters += special_string_length(str);
-            }
-            else if (*format == 'R')
-            {
-                
-                char *str = va_arg(my_list, char*);
-                print_rot13(str);
-                specific_characters += string_length(str);
-            }          
-
         }
         format++;
     }
@@ -186,3 +93,4 @@ int _printf(const char *format, ...)
 
     return specific_characters;
 }
+
--- main.h
+++ main.h
@@ -1,24 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
-#include <unistd.h>
 
 int _printf(const char *format, ...);
-int binary_length(unsigned int num);
-void print_binary(unsigned int num);
-
-void print_hexadecimal(unsigned long num, int base);
-int hex_length(unsigned long num);
-
-void print_special_string(char *str);
-int special_string_length(char *str);
-
-void print_rot13(char *str);
-int string_length(char *str);
 
 #endif /* MAIN_H */
 
